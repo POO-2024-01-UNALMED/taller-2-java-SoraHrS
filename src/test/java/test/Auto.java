b6a5f3f95aaf3eeb7a9efbc35da004794596e64f
SoraHrS <soherreras@unal.edu.co>
2024-02-18 19:54:30 -0500

    Se modificó la clase Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -22,8 +22,10 @@ public class Auto {
 	String verificarIntegridad() {
 		if (registro == motor.registro) {
 			for (int i = 0; i<asientos.length; i++) {
+				if (asientos[i] != null) {
 				if (asientos[i].registro != registro) {
 					return "Las piezas no son originales";
+					}
 				}
 			}
 			return "Auto original";
